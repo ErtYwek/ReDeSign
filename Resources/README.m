7ca39daee1b27373ba7f295557c522a631c6c451
ErtY'wek <34419000+ErtYwek@users.noreply.github.com>
2019-08-20 14:43:18 +0200

    ted2

--- Resources/README.m
+++ Resources/README.m
@@ -1,4 +1,4 @@
 So thats it
 and how it looks
-![Image](https://raw.githubusercontent.com/ErtYwek/ReDeSign/master/Resources/ErtYdesign.pngand how it looks)
-![Image of Yaktocat](https://octodex.github.com/images/yaktocat.png)
+![Image](https://raw.githubusercontent.com/ErtYwek/ReDeSign/master/Resources/ErtYdesign.png)
+
