bc37b6636fc87d571fdc17bb20284ce3681170f6
ErtY'wek <34419000+ErtYwek@users.noreply.github.com>
2019-08-20 14:43:49 +0200

    ted 3

--- Resources/README.m
+++ Resources/README.m
@@ -1,4 +1,4 @@
 So thats it
 and how it looks
-![Image](https://raw.githubusercontent.com/ErtYwek/ReDeSign/master/Resources/ErtYdesign.png)
+https://raw.githubusercontent.com/ErtYwek/ReDeSign/master/Resources/ErtYdesign.png
 
